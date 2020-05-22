d0d9d89002fe1d19b7ab8e73a814c01c029005e3
leobr <leobrionesherrera@gmail.com>
2020-05-22 12:47:12 -0500

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -5,7 +5,8 @@ class MusicLibraryController
     end
 
     def call
-        
+        input  = ""
+        until input == 'exit'
         puts "Welcome to your music library!"
         puts "To list all of your songs, enter 'list songs'."
         puts "To list all of the artists in your library, enter 'list artists'."
@@ -15,10 +16,23 @@ class MusicLibraryController
         puts "To play a song, enter 'play song'."
         puts "To quit, type 'exit'."
         puts "What would you like to do?"
-        input = nil
-        until input == "exit"
-            input = gets.strip
+        input = gets.strip
+        case input
+        when 'list songs'
+            list_songs
+        when 'list artists'
+            list_artists
+        when 'list genres'
+            list_genres
+        when 'list artist'
+            list_songs_by_artist
+        when 'list genre'
+            list_songs_by_genre
+        when 'play song'
+            play_song
         end
+    end
+
     end
 
     def list_songs
@@ -75,14 +89,6 @@ class MusicLibraryController
             index = input - 1
             puts "Playing #{sorted_songs[index].name} by #{sorted_songs[index].artist.name}" 
         end
-    end
-
-    def call
-        case input
-        when 'list songs'
-            list_songs
-        end
-
     end
 
 end
